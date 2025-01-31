c0c2944757548bb9dcb4333a82148f02376096ae
bpgsiform <maroallen.lago1@gmail.com>
2025-01-31 22:45:50 +0800

    Add files via upload

---
+++ functions/uploadToDrive.js
@@ -0,0 +1,37 @@
+const { google } = require('googleapis');
+const fs = require('fs');
+
+exports.handler = async (event, context) => {
+  try {
+    const auth = new google.auth.GoogleAuth({
+      keyFile: 'credentials.json',
+      scopes: ['https://www.googleapis.com/auth/drive'],
+    });
+
+    const drive = google.drive({ version: 'v3', auth });
+    const fileMetadata = {
+      name: 'invoice.pdf',
+      parents: ['1duVia6LZPGsESJiiYoHopyRiV5ypRm8-'], // Replace with your Google Drive folder ID
+    };
+    const media = {
+      mimeType: 'application/pdf',
+      body: fs.createReadStream('/tmp/invoice.pdf'),
+    };
+
+    const res = await drive.files.create({
+      resource: fileMetadata,
+      media: media,
+      fields: 'id',
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ fileId: res.data.id }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message }),
+    };
+  }
+};
